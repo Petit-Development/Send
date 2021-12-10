5561d6c0934193384bb393e6ebeb180434e67b84
Joshua Petitma <joshua@petit.dev>
2021-12-09 19:25:38 -0500

    Add firebase and upload functionality

--- pages/api/send.ts
+++ pages/api/send.ts
@@ -1,13 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import app from '../../secret/firebaseConfig';
+import 'firebase-admin/firestore';
+
+const firestore = app.firestore();
 
 type Data = {
-  name: string;
+  body: string;
 };
 
-export default function handler(
+type Error = {
+  error: string;
+  body?: string;
+};
+
+const requiredKeys = ['text', 'key'];
+
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Error | Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const missing = [];
+  for (const key of requiredKeys) {
+    if (!req.body.hasOwnProperty(key)) {
+      console.log(key);
+      missing.push(key);
+    }
+  }
+  if (missing.length != 0) {
+    console.log(missing);
+    res.status(422).json({ error: `Missing ${missing.join(', ')}` });
+  }
+  const upload = req.body;
+  try {
+    await firestore.doc(`data/${upload.key}`).set({ text: upload.text });
+  } catch {
+    res.status(400).json({ error: 'Error uploading text' });
+  }
+  res.status(200).json({ body: 'Uploaded!' });
 }
