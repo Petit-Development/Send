52350447a955fc6717dda52dedf779ab41c6ba42
Joshua Petitma <joshua@petit.dev>
2021-12-10 08:18:20 -0500

    Move download text field above download button

--- components/download/downloadCast.tsx
+++ components/download/downloadCast.tsx
@@ -18,6 +18,13 @@ export const DownloadCast: FC<DownloadCastProps> = ({
   return (
     <div className="bg-petit-grey z-0 flex flex-col justify-center items-center content-center bg-arrow-down h-full w-full bg-center bg-no-repeat bg-contain">
       <div className="flex flex-col justify-center w-full items-center place-items-center">
+        <div className="flex mt-10 justify-start w-96 h-10 bg-white rounded-2xl shadow-xl">
+          <input
+            className="flex-none p-3 border-transparent focus:outline-none w-11/12 h-10/12 bg-transparent resize-none"
+            placeholder="Enter Key..."
+            onChange={(change) => setKey(change.target.value)}
+          />
+        </div>
         <Button alt={true} text="Download" onClick={() => onDownload(key)} />
         <div
           className={`${
@@ -54,13 +61,6 @@ export const DownloadCast: FC<DownloadCastProps> = ({
           </div>
         </div>
 
-        <div className="flex mt-10 justify-start w-96 h-10 bg-white rounded-2xl shadow-xl">
-          <input
-            className="flex-none p-3 border-transparent focus:outline-none w-11/12 h-10/12 bg-transparent resize-none"
-            placeholder="Enter Key..."
-            onChange={(change) => setKey(change.target.value)}
-          />
-        </div>
       </div>
     </div>
   );
